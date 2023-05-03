8e314711376f6065573baefc8f80456923ba8626
Ameer Hamza <ah2050224@gmail.com>
2023-05-03 10:24:36 +0500

    Update sjf.py

--- Hamza Process Scheduler/sjf.py
+++ Hamza Process Scheduler/sjf.py
@@ -12,7 +12,11 @@ def sjf_np_scheduling(processes):
     processes = sorted(processes, key=lambda p: p.burst_time)
     n = len(processes)
     completed = [False] * n
-    current_time = 0
+    cur_time = 0
+    current_time = []
+    completion_time = []
+    turnaround_time = []
+    waiting_time = []
     total_turnaround_time = 0
     total_waiting_time = 0
     print("----------------------- SJF Non-Preemptive -----------------------")
@@ -23,7 +27,7 @@ def sjf_np_scheduling(processes):
         min_burst_time = float('inf')
         min_index = -1
         for i in range(n):
-            if not completed[i] and processes[i].arrival_time <= current_time and processes[i].burst_time < min_burst_time:
+            if not completed[i] and processes[i].arrival_time <= cur_time and processes[i].burst_time < min_burst_time:
                 min_burst_time = processes[i].burst_time
                 min_index = i
 
@@ -33,16 +37,20 @@ def sjf_np_scheduling(processes):
 
         # Execute the shortest job
         process = processes[min_index]
-        completion_time = current_time + process.burst_time
-        turnaround_time = completion_time - process.arrival_time
-        waiting_time = turnaround_time - process.burst_time
-        total_turnaround_time += turnaround_time
-        total_waiting_time += waiting_time
+        current_time.append(cur_time)
+        compl_time = cur_time + process.burst_time
+        completion_time.append(compl_time)
+        ta_time = compl_time - process.arrival_time
+        turnaround_time.append(ta_time)
+        wait_time = ta_time - process.burst_time
+        waiting_time.append(wait_time)
+        total_turnaround_time += ta_time
+        total_waiting_time += wait_time
         completed[min_index] = True
-        print(process.pid, "\t |", process.arrival_time, "\t |", process.burst_time, "\t |", completion_time, "\t |",turnaround_time, "\t |", waiting_time, " |")
+        print(process.pid, "\t |", process.arrival_time, "\t |", process.burst_time, "\t |", compl_time, "\t |",ta_time, "\t |", wait_time, " |")
 
         # Update the current time
-        current_time = completion_time
+        cur_time = compl_time
 
     # Calculate the average waiting time
     avg_waiting_time = total_waiting_time / n
@@ -52,19 +60,16 @@ def sjf_np_scheduling(processes):
     print("Average waiting time:", avg_waiting_time)
     print("Average turnaround time:", avg_turnaround_time)
 
-# class Process:
-#     def __init__(self, pid, arrival_time, burst_time):
-#         self.pid = pid
-#         self.arrival_time = arrival_time
-#         self.burst_time = burst_time
-#         self.remaining_time = burst_time
-
 def sjf_p_scheduling(processes):
     # Sort the processes based on their arrival time
     processes = sorted(processes, key=lambda p: p.arrival_time)
     n = len(processes)
     completed = [False] * n
-    current_time = 0
+    cur_time = 0
+    current_time = []
+    completion_time = []
+    turnaround_time = []
+    waiting_time = []
     total_turnaround_time = 0
     total_waiting_time = 0
     print("----------------------- SJF Preemptive -----------------------")
@@ -75,7 +80,7 @@ def sjf_p_scheduling(processes):
         min_remaining_time = float('inf')
         min_index = -1
         for i in range(n):
-            if not completed[i] and processes[i].arrival_time <= current_time and processes[i].remaining_time < min_remaining_time:
+            if not completed[i] and processes[i].arrival_time <= cur_time and processes[i].remaining_time < min_remaining_time:
                 min_remaining_time = processes[i].remaining_time
                 min_index = i
 
@@ -89,16 +94,20 @@ def sjf_p_scheduling(processes):
 
         # If the process has completed, calculate its completion time, turnaround time, and waiting time
         if process.remaining_time == 0:
-            completion_time = current_time + 1
-            turnaround_time = completion_time - process.arrival_time
-            waiting_time = turnaround_time - process.burst_time
-            total_turnaround_time += turnaround_time
-            total_waiting_time += waiting_time
+            current_time.append(cur_time)
+            compl_time = cur_time + 1
+            completion_time.append(compl_time)
+            ta_time = compl_time - process.arrival_time
+            turnaround_time.append(ta_time)
+            wait_time = ta_time - process.burst_time
+            waiting_time.append(wait_time)
+            total_turnaround_time += ta_time
+            total_waiting_time += wait_time
             completed[min_index] = True
-            print(process.pid, "\t |", process.arrival_time, "\t |", process.burst_time, "\t |", completion_time, "\t |",turnaround_time, "\t |", waiting_time, " |")
+            print(process.pid, "\t |", process.arrival_time, "\t |", process.burst_time, "\t |", compl_time, "\t |",ta_time, "\t |", wait_time, " |")
 
         # Update the current time
-        current_time += 1
+        cur_time += 1
 
     # Calculate the average waiting time
     avg_waiting_time = total_waiting_time / n
@@ -112,7 +121,6 @@ def sjf_p_scheduling(processes):
 # Example usage
 if __name__ == '__main__':
     
-    count = 0
     # Create some processes
     processes = [
         Process(1, 0, 10),
@@ -129,7 +137,6 @@ if __name__ == '__main__':
 
 	# Run the SJF Non-Preemptive scheduling algorithm
     sjf_np_scheduling(processes)
-print("\n\n")
-# Run the SJF Preemptive scheduling algorithm
-sjf_p_scheduling(processes)
-    # Print the average waiting time
+    print("\n\n")
+    # Run the SJF Preemptive scheduling algorithm
+    sjf_p_scheduling(processes)
