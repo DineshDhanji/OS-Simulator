f9687835b0c7056d528e1c253ab9cf0a20f0700d
Dinesh Dhanji <56204876+DineshDhanji@users.noreply.github.com>
2023-05-07 03:06:30 +0500

    Made few changes in structure of files

--- Python/Aneeq Page replacement algo.py
+++ Python/Aneeq Page replacement algo.py
@@ -176,6 +176,8 @@ def SecondChance(pages, pagefault, frame_size, page_frame):
 
 pages = [7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,7,1]
 pagefault = 0
-frame_size = int(input("Enter number of page frames: "))
+# frame_size = int(input("Enter number of page frames: "))
+frame_size = 3
 page_frame = []
+FIFO(pages, pagefault, frame_size, page_frame)
 # SecondChance(pages , pagefault , frame_size , page_frame) Test any function here no need to change the parameters
